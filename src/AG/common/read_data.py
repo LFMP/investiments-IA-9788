d0f1b6a33c247abb43336211f59b35d8ff51fabc
LFMP <luizfellipe42@hotmail.com>
2020-10-04 17:17:11 -0300

    add func to read 2014-2015 files

---
+++ src/AG/common/read_data.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+
+def read_data2mov():
+    companys_files = [x for x in os.listdir(os.getcwd() + '/src/common/')]
+
+    companys = [x.split('-', 1)[0] for x in companys_files if '2014-2015' in x]
+    companys_14_15_files = [x for x in companys_files if '2014-2015' in x]
+
+    data = {}
+    header = []
+    for filename in companys_14_15_files:
+        path = os.getcwd() + '/src/common/' + filename
+        with open(path, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            data[filename[:-4]] = []
+            for (index, row) in enumerate(spamreader):
+                if(index == 0):
+                    header = row
+                    for column_name in header:
+                        data[filename[:-4]].append({column_name: []})
+                else:
+                    for (i, value) in enumerate(row):
+                        data[filename[:-4]][i][header[i]].append(value)
+    return data
