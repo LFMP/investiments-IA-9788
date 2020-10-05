ad90821d43caca2dc545e9421a5a40c5f071d7ba
LFMP <luizfellipe42@hotmail.com>
2020-10-05 20:56:54 -0300

    adapt to class Individuo

--- src/AG/util.py
+++ src/AG/util.py
@@ -3,6 +3,17 @@ import numpy as np
 from math import ceil
 
 
+class Individuo:
+    def __init__(self, vetor, ft=0):
+        self.vetor = vetor
+        self.ft = ft
+
+    def fitness(self, weights):
+        for (index, value) in enumerate(self.vetor):
+            self.ft += np.sum(np.multiply(weights[index], value))
+        self.ft = self.ft/np.sum(self.vetor)
+
+
 def desempenho(empresa):
     openValues = empresa[1]
     highValues = empresa[2]
@@ -23,31 +34,26 @@ def weigthEmpresas(empresas):
     return weights
 
 
-def fitness(individuo, weights):
-    fitv = 0
-    for (index, value) in enumerate(individuo):
-        fitv += np.sum(np.multiply(weights[index], value))
-    return fitv/np.sum(individuo)
-
-
 def fixSum(individuo):
-    dist = np.sum(individuo)
+    dist = np.sum(individuo.vetor)
     if (dist > 1.0):
         discount = dist - 1.0
-        for (index, value) in enumerate(individuo):
+        for (index, value) in enumerate(individuo.vetor):
             if(value >= discount):
-                individuo[index] = value - discount
+                individuo.vetor[index] = value - discount
                 break
     elif(dist < 1.0):
         increase = 1.0 - dist
-        individuo[random.randrange(len(individuo))] += increase
+        individuo.vetor[random.randrange(len(individuo))] += increase
 
 
 def geraIndividuo(qtdElementos):
     proporcoes = np.zeros(qtdElementos)
     while(np.sum(proporcoes) != 1.0):
         proporcoes[random.randrange(qtdElementos)] = random.randrange(100)/100
-    return proporcoes
+    ind = Individuo(proporcoes)
+    ind.fitness()
+    return ind
 
 
 def geraPopulacao(qtdIndividuos, qtdElementos, populacao):
@@ -57,27 +63,34 @@ def geraPopulacao(qtdIndividuos, qtdElementos, populacao):
 
 
 def mutacao(individuo):
-    size = len(individuo)
+    size = len(individuo.vetor)
     qtd_mutacoes = random.randrange(ceil(size/3))
     random.seed(random.random())
     for i in range(qtd_mutacoes):
-        individuo[random.randrange(size-1)] = random.randrange(100)/100
+        individuo.vetor[random.randrange(size-1)] = random.randrange(100)/100
     fixSum(individuo)
 
 
-def cruzamento(mae, pai, qtdFIlhos=2):
+def cruzamento(mae, pai, qtdFIlhos=6):
     random.seed(random.random())
     cortes = [random.randrange(len(mae)-1) for x in range(2)]
     cortes.sort()
     filhos = []
-    partesMae = np.array_split(mae, cortes)
-    partesPai = np.array_split(pai, cortes)
-    filhos.append(np.concatenate(partesPai[0], partesMae[1], partesMae[2]))
-    filhos.append(np.concatenate(partesMae[0], partesPai[1], partesPai[2]))
-    filhos.append(np.concatenate(partesMae[0], partesPai[1], partesMae[2]))
-    filhos.append(np.concatenate(partesPai[0], partesMae[1], partesPai[2]))
-    filhos.append(np.concatenate(partesMae[0], partesMae[1], partesPai[2]))
-    filhos.append(np.concatenate(partesPai[0], partesPai[1], partesMae[2]))
-    for f in filhos:
-        fixSum(f)
+    partesMae = np.array_split(mae.vetor, cortes)
+    partesPai = np.array_split(pai.vetor, cortes)
+    filhos.append(Individuo(np.concatenate(
+        partesPai[0], partesMae[1], partesMae[2])))
+    filhos.append(Individuo(np.concatenate(
+        partesMae[0], partesPai[1], partesPai[2])))
+    filhos.append(Individuo(np.concatenate(
+        partesMae[0], partesPai[1], partesMae[2])))
+    filhos.append(Individuo(np.concatenate(
+        partesPai[0], partesMae[1], partesPai[2])))
+    filhos.append(Individuo(np.concatenate(
+        partesMae[0], partesMae[1], partesPai[2])))
+    filhos.append(Individuo(np.concatenate(
+        partesPai[0], partesPai[1], partesMae[2])))
+    for i in range(qtdFIlhos):
+        fixSum(filhos[i])
+        filhos[i].fitness()
     return filhos
