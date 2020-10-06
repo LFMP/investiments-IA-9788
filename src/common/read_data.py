0e6688615abd93abbe0a5d5c7334a7dafaf34f19
LFMP <luizfellipe42@hotmail.com>
2020-10-06 18:22:47 -0300

    new way to read

--- src/common/read_data.py
+++ src/common/read_data.py
@@ -4,7 +4,6 @@ import os
 
 def read_data2mov():
     companys_files = [x for x in os.listdir(os.getcwd() + '/src/database/')]
-    companys = [x.split('-', 1)[0] for x in companys_files if '2014-2015' in x]
     companys_14_15_files = [x for x in companys_files if '2014-2015' in x]
 
     data = {}
@@ -13,13 +12,13 @@ def read_data2mov():
         path = os.getcwd() + '/src/database/' + filename
         with open(path, newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',')
-            data[filename[:-8]] = []
+            data[filename[:-8]] = {}
             for (index, row) in enumerate(spamreader):
                 if(index == 0):
                     header = row
                     for column_name in header:
-                        data[filename[:-8]].append({column_name: []})
+                        data[filename[:-8]][column_name] = []
                 else:
                     for (i, value) in enumerate(row):
-                        data[filename[:-8]][i][header[i]].append(value)
+                        data[filename[:-8]][header[i]].append(value)
     return data
